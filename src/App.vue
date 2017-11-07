048389129aab507200a0887843ca2109ffc6ef45
NamHyun Gu <mnhan0403@gmail.com>
2017-11-07 14:30:17 +0900

    Add vue-material

--- src/App.vue
+++ src/App.vue
@@ -6,18 +6,21 @@
 </template>
 
 <script>
-export default {
-  name: 'app',
-};
+  export default {
+    name: 'app',
+  };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import url(https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic);
+  @import url(https://fonts.googleapis.com/icon?family=Material+Icons);
+
+  #app {
+    font-family: 'Roboto', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
