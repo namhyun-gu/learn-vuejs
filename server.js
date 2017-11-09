516b29f1f2c857f9cda9b32f6042fabd4f4e9692
NamHyun Gu <mnhan0403@gmail.com>
2017-11-09 17:37:04 +0900

    Support history mode in server
    - Added connect-history-api-fallback middleware to express.

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const serveStatic = require('serve-static');
+const history = require('connect-history-api-fallback');
 
 const app = express();
+app.use(history());
 app.use(serveStatic(`${__dirname}/dist`));
 
 const port = process.env.PORT || 5000;
