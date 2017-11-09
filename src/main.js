d92d20b9874c52d3c0556c8954cae36d382c8c8f
NamHyun Gu <mnhan0403@gmail.com>
2017-11-09 09:31:36 +0900

    Add axios http client
    - Axios Document (https://github.com/axios/axios)

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import axios from 'axios';
 import VueMaterial from 'vue-material';
 import 'vue-material/dist/vue-material.css';
 
@@ -8,6 +9,8 @@ import App from './App';
 import router from './router';
 
 Vue.config.productionTip = false;
+
+Vue.prototype.$http = axios;
 
 Vue.use(VueMaterial);
 
