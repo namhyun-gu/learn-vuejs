cdcae2c0db3cb4ff9e8e0f88400801937d3807f8
NamHyun Gu <mnhan0403@gmail.com>
2017-11-10 12:30:28 +0900

    Add loading spinner

--- src/components/TimesViewer/NewsList.vue
+++ src/components/TimesViewer/NewsList.vue
@@ -1,24 +1,25 @@
 <template>
   <div class="list">
-    <template v-if="newsList.length != 0">
+    <template v-if="!isShowSpinner && newsList.length != 0">
       <md-card v-for="news in newsList" :key="news.id">
         <md-card-header>
           <div class="md-title">{{ news.title }}</div>
           <div class="md-subhead" v-if="news.byline !== ''">{{ news.byline }} ({{ news.section }})</div>
           <div class="md-subhead" v-else>{{ news.section }}</div>
         </md-card-header>
-
         <md-card-content>
           {{ news.abstract }}
         </md-card-content>
-
         <md-card-actions>
           <md-button class="md-accent" target="_blank" :href="news.url">Open</md-button>
         </md-card-actions>
       </md-card>
     </template>
+    <template v-else-if="isShowSpinner">
+      <md-spinner md-indeterminate class="md-accent center"></md-spinner>
+    </template>
     <template v-else>
-      <span class="error-message">No content</span>
+      <span class="center">No content</span>
     </template>
   </div>
 </template>
@@ -45,6 +46,7 @@
     data() {
       return {
         newsList: [],
+        isShowSpinner: false,
       };
     },
     created() {
@@ -61,13 +63,16 @@
     methods: {
       fetchNews() {
         const baseUrl = 'https://api.nytimes.com/svc/news/v3';
+        this.isShowSpinner = true;
         this.$http
             .get(`${baseUrl}/content/${this.source}/${this.section}/.json?api-key=${this.api_key}`)
             .then((result) => {
               this.newsList = result.data.results;
+              this.isShowSpinner = false;
             })
             .catch(() => {
               this.newsList = [];
+              this.isShowSpinner = false;
             });
       },
     },
@@ -75,7 +80,12 @@
 </script>
 
 <style scoped>
-  .list {
+  .center {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin: 0 -50% 0 0;
+    transform: translate(-50%, -50%)
   }
 
   .md-card {
